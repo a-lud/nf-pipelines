ac6e2dc08ee6db93ae59c90e422cb8251e58a406
a-lud <alastair.j.lud@gmail.com>
2022-05-27 16:09:33 +0930

    No longer have if-conditional in help

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -5,41 +5,39 @@ class WorkflowMain {
     // help Prints the help text to screen
     public static String help(parameters, workflow) {
         
-        if (parameters.help) {
-            // Coloured output
-            def c_reset = "\033[0m"
-            def c_green = "\033[0;32m"
-            def c_yellow = "\033[0;33m"
+        // Coloured output
+        def c_reset = "\033[0m"
+        def c_green = "\033[0;32m"
+        def c_yellow = "\033[0;33m"
 
-            // Parse pipeline schema as map - definitions of what arguments should be
-            def strJson = new File("${workflow.projectDir}/nextflow_schema.json").text
-            def Map mapSchema = (Map) new JsonSlurper().parseText(strJson)
+        // Parse pipeline schema as map - definitions of what arguments should be
+        def strJson = new File("${workflow.projectDir}/nextflow_schema.json").text
+        def Map mapSchema = (Map) new JsonSlurper().parseText(strJson)
 
-            // Title for meta information
-            def pipelineMeta = mapSchema.subMap([ 'version', 'title', 'description' ])
-            def line1 = "\n" + "=" * 80 + "\n"
-            def title = c_yellow + pipelineMeta['title'] + ' ' + pipelineMeta['version'] + c_reset
-            def centerPoint = (80 - title.length())/2 + 4
-            println(
-                line1 + ' ' * centerPoint + title + line1 + 
-                '\n' + pipelineMeta['description'].join('\n') + '\n'
-            )
+        // Title for meta information
+        def pipelineMeta = mapSchema.subMap([ 'version', 'title', 'description' ])
+        def line1 = "\n" + "=" * 80 + "\n"
+        def title = c_yellow + pipelineMeta['title'] + ' ' + pipelineMeta['version'] + c_reset
+        def centerPoint = (80 - title.length())/2 + 4
+        println(
+            line1 + ' ' * centerPoint + title + line1 + 
+            '\n' + pipelineMeta['description'].join('\n') + '\n'
+        )
 
-            // Defualt help page - or help page for sub-workflow too?
-            def helpSubset = parameters.help instanceof String ? 
-                mapSchema.get('definitions').subMap(['mandatory', parameters.help]) : 
-                mapSchema.get('definitions').subMap(mapSchema.get('definitions').keySet( ))
+        // Defualt help page - or help page for sub-workflow too?
+        def helpSubset = parameters.help instanceof String ? 
+            mapSchema.get('definitions').subMap(['mandatory', 'cluster', parameters.help]) : 
+            mapSchema.get('definitions').subMap(mapSchema.get('definitions').keySet( ))
 
-            // Print the help pages nicely
-            prettyPrint(helpSubset)
+        // Print the help pages nicely
+        prettyPrint(helpSubset)
 
-            // 'false' is created when help is run - delete it!
-            def temp_outdir = new File('./false')
-            if (temp_outdir.exists()) {
-                temp_outdir.deleteDir()
-            }
-            System.exit(0)
+        // 'false' is created when help is run - delete it!
+        def temp_outdir = new File('./false')
+        if (temp_outdir.exists()) {
+            temp_outdir.deleteDir()
         }
+        System.exit(0)
     }
 
     // prettyprint Helper function to print the help page nicely
