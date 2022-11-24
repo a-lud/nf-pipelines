124b0441e8672b6a4aa261b01ec58a03fcbe7758
a-lud <alastair.j.lud@gmail.com>
2022-11-25 08:17:12 +1030

    Use ETE3 rather than Cogent3 (easier install for older systems)

--- bin/remove_foreground.py
+++ bin/remove_foreground.py
@@ -2,7 +2,7 @@
 
 import argparse
 import logging
-from cogent3 import load_tree
+from ete3 import Tree
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(levelname)-8s %(message)s"
@@ -13,7 +13,7 @@ def getArgs():
     """Get user arguments and set up parser"""
     desc = """\
     # -------------------------------------------------------- #
-    #                ETE3 Evol output-to-table                 #
+    #              ETE3: Remove foreground branches            #
     # -------------------------------------------------------- #
     Remove foreground samples from a Newick tree of Site-model
     testing.
@@ -31,7 +31,7 @@ def getArgs():
         description=desc,
         epilog=epi,
     )
-
+    
     # Required, positional input file arguments
     parser.add_argument(
         "tree",
@@ -49,33 +49,34 @@ def getArgs():
 if __name__ == "__main__":
     args = getArgs()
 
-    # Marked tree
-    tree = load_tree(args.tree)
-    logging.info(f"Original tree:\n")
-    print(tree.ascii_art())
-    print("\n" + "-" * 100 + "\n")
+    # Original tree
+    logging.info("Importing tree (format 1)")
+    tree = Tree(args.tree, format=1)
 
-    # Get oldest marked node
-    nodes = [ l for l in tree.get_node_names() if '#' in l ]
-    len_tips = 0
-    tips = None
-    for n in nodes:
-        t = tree.get_node_matching_name(n).tips()
-        if len_tips < len(t):
-            len_tips = len(t)
-            tips = t
+    logging.info(f"Initial tree:\n{tree}")
 
-    # Prune tips from tree (will automatically remove single internal nodes)
-    for t in tips:
-        print(f"\t- {t.name}")
-        t.parent.remove(t)
-        tree.prune()
+    # Find which node to remove based on length
+    logging.info("Removing marked Nodes/Leaves")
+    for n in tree.traverse('postorder'):
+        if '#' in n.name:
+            if n.is_leaf():
+                n.detach()
+            else:
+                n.delete()
+
+    # Clean up any internal nodes that have now been made leaves through pruning.
+    logging.info("Removing internal nodes that are now leaves without children")
+    for n in tree.traverse('postorder'):
+        if n.is_leaf() and n.name == '':
+            n.detach()
+
+    logging.info("Deleting internal nodes that only have one child (i.e. remove node and keep leaf)")
+    for n in tree.traverse('postorder'):
+        if not n.is_leaf() and len(n.children) < 2:
+            n.delete()
 
     # Post trimming
-    print()
-    logging.info("Final tree:")
-    print("\n" + tree.ascii_art(show_internal=False))
-    print("\n" + "-" * 100 + "\n")
+    logging.info(f"Final tree:\n{tree}")
 
-    # Write new tree to file
-    tree.write(args.output)
+    # # Write new tree to file
+    tree.write(format=1, outfile=args.output)
